1e3ae7d1e7d7a35097a3c3ff607051b44ddc0517
crishard <crislantorres9@gmail.com>
2024-11-28 11:26:24 -0300

    feature: adicionando validação de usuário logado

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,32 +1,36 @@
 import { GraduationCap } from 'lucide-react';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { LoginForm } from '../../components/auth/LoginForm';
+import { useAuth } from '../../context/AuthContext';
 
 export const LoginPage: React.FC = () => {
+    const { user } = useAuth();
+    if (user) {
+        return <Navigate to="/" replace />;
+    }
+    return (
+        <div className=" bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="flex justify-center">
+                    <GraduationCap className="h-12 w-12 text-blue-600" />
+                </div>
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                    Entre na sua conta
+                </h2>
+                <p className="mt-2 text-center text-sm text-gray-600">
+                    Ou{' '}
+                    <Link to="/register" className="font-medium text-blue-600 hover:text-blue-500">
+                        crie uma nova conta
+                    </Link>
+                </p>
+            </div>
 
-  return (
-    <div className=" bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="flex justify-center">
-          <GraduationCap className="h-12 w-12 text-blue-600" />
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <LoginForm />
+                </div>
+            </div>
         </div>
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Entre na sua conta
-        </h2>
-        <p className="mt-2 text-center text-sm text-gray-600">
-          Ou{' '}
-          <Link to="/register" className="font-medium text-blue-600 hover:text-blue-500">
-            crie uma nova conta
-          </Link>
-        </p>
-      </div>
-
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <LoginForm />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,32 +1,37 @@
 import { GraduationCap } from 'lucide-react';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { RegisterForm } from '../../components/auth/RegisterForm';
+import { useAuth } from '../../context/AuthContext';
 
 export const RegisterPage: React.FC = () => {
+    const { user } = useAuth();
 
-  return (
-    <div className=" bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="flex justify-center">
-          <GraduationCap className="h-12 w-12 text-blue-600" />
-        </div>
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Crie sua conta
-        </h2>
-        <p className="mt-2 text-center text-sm text-gray-600">
-          Já tem uma conta?{' '}
-          <Link to="/login" className="font-medium text-blue-600 hover:text-blue-500">
-            Faça login
-          </Link>
-        </p>
-      </div>
+    if (user) {
+        return <Navigate to="/" replace />;
+    }
+    return (
+        <div className=" bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="flex justify-center">
+                    <GraduationCap className="h-12 w-12 text-blue-600" />
+                </div>
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                    Crie sua conta
+                </h2>
+                <p className="mt-2 text-center text-sm text-gray-600">
+                    Já tem uma conta?{' '}
+                    <Link to="/login" className="font-medium text-blue-600 hover:text-blue-500">
+                        Faça login
+                    </Link>
+                </p>
+            </div>
 
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <RegisterForm />
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <RegisterForm />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
