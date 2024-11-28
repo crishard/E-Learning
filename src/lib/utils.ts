b7f27b5be1245883babff905225e8e97484026da
crishard <crislantorres9@gmail.com>
2024-11-28 09:46:17 -0300

    feature: criando utilitário para formatação do preço

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,12 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
+}
+
+export function formatPrice(price: number): string {
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    }).format(price);
 }
