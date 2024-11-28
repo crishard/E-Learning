4fd4a4f3a6fb0cc69501c3df14f227a8c922027d
crishard <crislantorres9@gmail.com>
2024-11-28 09:32:42 -0300

    feature: criando base da  homepage do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { LoginPage } from "./pages/auth/Login";
 import { RegisterPage } from "./pages/auth/Register";
+import { HomePage } from './pages/HomePage';
 function App() {
 
   return (
@@ -8,6 +9,7 @@ function App() {
         <div className="min-h-screen bg-gray-50">
           <main className="max-w-7xl mx-auto px-4 py-8">
             <Routes>
+            <Route path="/" element={<HomePage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
             </Routes>
