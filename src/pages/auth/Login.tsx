d11a3ef26dd2fe65affa2a94a6b6a8a602345fae
crishard <crislantorres9@gmail.com>
2024-11-28 10:41:56 -0300

    style: ajuste de altura

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -6,7 +6,7 @@ import { LoginForm } from '../../components/auth/LoginForm';
 export const LoginPage: React.FC = () => {
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className=" bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <div className="flex justify-center">
           <GraduationCap className="h-12 w-12 text-blue-600" />
