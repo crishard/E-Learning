2f413eb14518a24ccd4632cb720e4ffbd6c17e29
crishard <crislantorres9@gmail.com>
2024-11-28 09:46:56 -0300

    feature: criando store do carrinho

---
+++ src/store/cartStore.ts
@@ -0,0 +1,37 @@
+import { create } from 'zustand';
+import { Cart, CartItem } from '../types/cart';
+
+interface CartStore {
+  cart: Cart;
+  addItem: (item: CartItem) => void;
+  removeItem: (courseId: string) => void;
+  clearCart: () => void;
+}
+
+export const useCartStore = create<CartStore>((set) => ({
+  cart: { items: [], total: 0 },
+  addItem: (item) =>
+    set((state) => {
+      if (state.cart.items.some((i) => i.courseId === item.courseId)) {
+        return state;
+      }
+      const newItems = [...state.cart.items, item];
+      return {
+        cart: {
+          items: newItems,
+          total: newItems.reduce((sum, item) => sum + item.price, 0),
+        },
+      };
+    }),
+  removeItem: (courseId) =>
+    set((state) => {
+      const newItems = state.cart.items.filter((item) => item.courseId !== courseId);
+      return {
+        cart: {
+          items: newItems,
+          total: newItems.reduce((sum, item) => sum + item.price, 0),
+        },
+      };
+    }),
+  clearCart: () => set({ cart: { items: [], total: 0 } }),
+}));
