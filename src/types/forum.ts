b2d37079734a46bcc4e344f08551e958419e6d5c
crishard <crislantorres9@gmail.com>
2024-12-04 10:47:24 -0300

    feature: alteração no tipo dos dados do forum

--- src/types/forum.ts
+++ src/types/forum.ts
@@ -1,23 +1,23 @@
 export interface ForumPost {
-    id: string;
-    courseId: string;
-    lessonId: string;
-    userId: string;
-    userDisplayName: string;
-    userPhotoURL?: string;
-    content: string;
-    createdAt: Date;
-    updatedAt?: Date;
-    replies: ForumReply[];
-  }
-  
-  export interface ForumReply {
-    id: string;
-    postId: string;
-    userId: string;
-    userDisplayName: string;
-    userPhotoURL?: string;
-    content: string;
-    createdAt: Date;
-    updatedAt?: Date;
-  }
+  id: string;
+  courseId: string;
+  lessonId: string;
+  userId: string;
+  userDisplayName: string;
+  userPhotoURL?: string;
+  content: string;
+  createdAt: Date;
+  updatedAt?: Date;
+  replies: ForumReply[];
+}
+
+export interface ForumReply {
+  id: string;
+  postId: string;
+  userId: string;
+  userDisplayName: string;
+  userPhotoURL?: string;
+  content: string;
+  createdAt: Date;
+  updatedAt?: Date;
+}
